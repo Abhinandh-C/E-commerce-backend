5d9d5049cc7bc21fb588bf0b934dffe2ee9e1576
Abhinandh <abhinandh20011@gmail.com>
2025-01-13 12:40:55 +0530

    cart schema model

---
+++ model/cartschema.js
@@ -0,0 +1,33 @@
+const mongoose = require ('mongoose')
+
+const cartschema = mongoose.Schema({
+      userid : {
+           type : mongoose.Schema.Types.ObjectId,
+           ref : "user"
+       },
+       products: [{
+
+        productid : {
+           type : mongoose.Schema.Types.ObjectId,
+           ref : "product"
+       },
+       quantity : {
+            type:String,
+             default:1
+       },
+        price : {
+             type:Number
+        },
+        totalamount : {
+          type:Number
+     }
+
+             
+       }],
+       Netamount : {
+            type:Number
+       }
+
+    })
+
+    module.exports = mongoose.model('cart',cartschema)
