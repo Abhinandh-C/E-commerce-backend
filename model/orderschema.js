3f458963d281e6933377308881cff0bda3dc8d42
Abhinandh <abhinandh20011@gmail.com>
2025-01-13 12:42:03 +0530

    order schema model

---
+++ model/orderschema.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const address = require('./address')
+
+const orderschema = mongoose.Schema({
+
+     userid: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+     },
+     products: [{
+     
+             productid : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "product"
+            },
+            quantity : {
+                 type:String,
+                  default:1
+            },
+             price : {
+                  type:Number
+             },
+             totalamount : {
+               type:Number
+          }
+             
+            }],
+    Netamount : {
+             type:Number
+              },
+              totalPriceAfterDiscount : {
+                type:Number
+                 },
+    address : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "addresses"
+
+    },
+    Status : {
+            type: String,
+            enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+            default: 'Pending'
+    },
+    paymentMethod: {
+        type : String,
+        enum : ['COD','OnlinePayment'],
+        default : true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['Pending', 'Completed', 'Failed', 'Refunded'],
+        default: 'Pending'
+    },
+    
+})
+
+
+module.exports =  mongoose.model('order',orderschema)
