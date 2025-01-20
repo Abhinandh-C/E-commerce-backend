85438f32d716387c6d53ee561b1efbac3342a29e
Abhinandh <abhinandh20011@gmail.com>
2025-01-20 17:08:09 +0530

    usermanagement module has been completed

--- Router/admin-usermanagement.js
+++ Router/admin-usermanagement.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router()
 const {viewuser,viewsingleuser, deleteuser, blockuser,viewblockuser} = require('../admincontroller/usermanagement')
+const verifyToken = require('../middleware/tokenverification')
+const blacklist = require('../middleware/blacklist')
 
 
-router.get('/admin/viewuser',viewuser)
-router.get('/admin/viewsingleuser/:id',viewsingleuser)
-router.delete('/admin/deleteuser/:id',deleteuser)
-router.put('/admin/blockuser/:id',blockuser)
-router.get('/admin/viewblockuser',viewblockuser)
+router.get('/admin/viewuser',verifyToken,blacklist,viewuser)
+router.get('/admin/viewsingleuser/:id',verifyToken,blacklist,viewsingleuser)
+router.delete('/admin/deleteuser/:id',verifyToken,blacklist,deleteuser)
+router.put('/admin/blockuser/:id',verifyToken,blacklist,blockuser)
+router.get('/admin/viewblockuser',verifyToken,blacklist,viewblockuser)
 
 module.exports=router
 
--- admincontroller/usermanagement.js
+++ admincontroller/usermanagement.js
@@ -1,94 +1,109 @@
 const Schema = require('../model/userschema')
 
 
-const viewuser = async (req,res) => {
+const viewuser = async (req, res) => {
     try {
+        //find from schema
         const user = await Schema.find({})
 
+        //checking the user
         if (!user) {
-            return res.status(404).json({message: "user was not found in the database"})
+            return res.status(404).json({ message: "user was not found in the database" })
         }
-        return res.status(200).json({message: "List of the users",user})
+        return res.status(200).json({ message: "List of the users", user })
 
-        
-        } catch (error) {
-            return res.status(400).json({message: "invalid command"})
-        
-    }
-    
-}
-
-
-const viewsingleuser = async (req,res) => {
-    try {
-        const userid = req.params.id;
-        const user = await Schema.findById(userid)
-       
-        if (!user) {
-            return res.status(404).json({message: "user was not found in the database"})
-        }
-        return res.status(200).json({message: "List of the users",user})
-
-
+        //error message
     } catch (error) {
-        return res.status(400).json({message: "invalid command"})
+        return res.status(400).json({ message: "invalid command" })
     }
-    
+
 }
 
-const deleteuser = async (req,res) => {
+
+const viewsingleuser = async (req, res) => {
     try {
+        //taking from params
         const userid = req.params.id;
+
+        //finding userId from schema
+        const user = await Schema.findById(userid)
+
+        //checking the user
+        if (!user) {
+            return res.status(404).json({ message: "user was not found in the database" })
+        }
+        return res.status(200).json({ message: "List of the users", user })
+
+        //error message
+    } catch (error) {
+        return res.status(400).json({ message: "invalid command" })
+    }
+
+}
+
+const deleteuser = async (req, res) => {
+    try {
+        //taking from params
+        const userid = req.params.id;
+
+        //finding userID and delete from schema
         const user = await Schema.findByIdAndDelete(userid)
 
- 
+        //checking the user
         if (!user) {
-            return res.status(404).json({message: "user was not found "})
+            return res.status(404).json({ message: "user was not found " })
         }
-        return res.status(200).json({message: "The user has been deleted",user})
+        return res.status(200).json({ message: "The user has been deleted", user })
 
-        
+        //error message
     } catch (error) {
-        return res.status(400).json({message: "invalid command"})
+        return res.status(400).json({ message: "invalid command" })
     }
-    
+
 }
 
 
-const blockuser = async (req,res) => {
+const blockuser = async (req, res) => {
     try {
+        //taking from params
         const userid = req.params.id;
-        await Schema.findByIdAndUpdate(userid,{active:false})
-        
-        
+
+        //find userID from schema and update to active false
+        await Schema.findByIdAndUpdate(userid, { active: false })
+
+        //checking the user
         if (!user) {
-            return res.status(404).json({message: "user was not found "})
+            return res.status(404).json({ message: "user was not found " })
         }
-        return res.status(200).json({message: "The user has been Blocked",user})
+        return res.status(200).json({ message: "The user has been Blocked", user })
 
+        //error message
     } catch (error) {
-        return res.status(400).json({message: "invalid command"})
+        return res.status(400).json({ message: "invalid command" })
     }
-    
+
 }
-    
-const viewblockuser = async (req,res) => {
+
+const viewblockuser = async (req, res) => {
     try {
-        const blockuser = await Schema.find({active:false})
-        
-        if (!blockuser) {
-            return res.status(404).json({message: "not found "})
-        }
-        return res.status(200).json({message: "Blocked users",blockuser})
+        //find active : false , in  schema
+        const blockuser = await Schema.find({ active: false })
 
+        //checking the blockuser
+        if (!blockuser) {
+            return res.status(404).json({ message: "not found " })
+        }
+        return res.status(200).json({ message: "Blocked users", blockuser })
+
+        //error message
     } catch (error) {
-        return res.status(400).json({message: "invalid command"})
+        return res.status(400).json({ message: "invalid command" })
     }
-    
+
 }
 
 
-module.exports={viewuser,viewsingleuser,deleteuser,blockuser,viewblockuser}
+module.exports = { viewuser, viewsingleuser, deleteuser, blockuser, viewblockuser }
 
 
-    
+
