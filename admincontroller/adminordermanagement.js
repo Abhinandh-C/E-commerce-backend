199ccab8b485e6a549af5a67de90e1403f84f638
Abhinandh <abhinandh20011@gmail.com>
2025-01-29 15:06:08 +0530

    order management complete  module for admin and user

--- admincontroller/adminordermanagement.js
+++ admincontroller/adminordermanagement.js
@@ -2,23 +2,23 @@ const orderschema = require('../model/orderschema')
 
 
 
-const deleteorder = async (req,res) => {
+const deleteorder = async (req, res) => {
     try {
         //checking the userid
         const orderid = req.params.id
 
         //checking the order
         const order = await orderschema.findByIdAndDelete(orderid)
-        return res.status(200).json({message: 'order has been deleted'})
+        return res.status(200).json({ message: 'order has been deleted' })
 
-    //error handling    
+        //error handling    
     } catch (error) {
-        return res.status(400).json({message:'something went wrong'})
+        return res.status(400).json({ message: 'something went wrong' })
     }
-    
+
 }
 
-const viewallorder = async (req,res) => {
+const viewallorder = async (req, res) => {
     try {
         //getting the user details
         const userid = req.user.id
@@ -29,47 +29,50 @@ const viewallorder = async (req,res) => {
 
         //check the order is been existing
         if (!orderdetails) {
-            return res.status(404).json({message: ' order is not found'})
-            
+            return res.status(404).json({ message: ' order is not found' })
+
         }
         //order success
-        return res.status(200).json({message: 'order details',orderdetails})
-    
-    //error 
+        return res.status(200).json({ message: 'order details', orderdetails })
+
+        //error 
     } catch (error) {
-        return res.status(400).json({message: ' error',error:error.message})
+        return res.status(400).json({ message: ' error', error: error.message })
     }
-    
+
 }
 
 
-const updatestatus=async (req,res) => {
+const updatestatus = async (req, res) => {
     try {
         //check the order
         const orderId = req.params.id
 
         //getting from the body
-        const {Status} = req.body;
+        const { Status } = req.body;
 
         //check the order
         if (!orderId) {
-            return res.status(404).json({message: "order not found"})
+            return res.status(404).json({ message: "order not found" })
         }
-        
+
         //find and update
         const update = await orderschema.findByIdAndUpdate(orderId,
-            {Status} ,
-            {new:true}
+            { Status },
+            { new: true }
         );
-                if (!update) {
-                    return res.status(400).json({message: "cannot update the coupon"});
-                }
-                res.status(200).json({message: "updated successfully",update});
-        
-         } catch (error) {
-            res.status(400).json({message: "failed to update ",error: error.message })
+
+        //checking the condition
+        if (!update) {
+            return res.status(400).json({ message: "cannot update the coupon" });
         }
-    
+        res.status(200).json({ message: "updated successfully", update });
+
+        //error command
+    } catch (error) {
+        res.status(400).json({ message: "failed to update ", error: error.message })
+    }
+
 }
 
-module.exports={deleteorder,viewallorder,updatestatus}
+module.exports = { deleteorder, viewallorder, updatestatus }
--- controller/ordermanagement.js
+++ controller/ordermanagement.js
@@ -5,167 +5,161 @@ const instance = require('../instance_razorpay');
 const nodemailer = require('nodemailer');
 const userschema = require("../model/userschema");
 require('dotenv').config()
-// const { default: product } = require("razorpay/dist/types/products");
-// const { default: products } = require("razorpay/dist/types/products");
 
-const placeanorder = async (req,res) => {
+const placeanorder = async (req, res) => {
     try {
         //getting from body
-        const{ paymentMethod } = req.body;
+        const { paymentMethod } = req.body;
 
+        //checking the user by the token id
         const userid = req.user.id;
 
+        //finding the userid from schema
         const name = await userschema.findById(userid)
-        //console.log("name", name);
-        
 
         // checking the address
-        const address = await addressschema.findOne({userid})
-        if ( !address ) { 
+        const address = await addressschema.findOne({ userid })
+        if (!address) {
             return res.status(400).json({
                 message: 'need a valid address'
             })
         }
 
- 
         //checking the payment method
         if (!paymentMethod || [!'COD' || !'OnlinePayment'].includes(paymentMethod)) {
-            return res.status(400).json({message: 'please select any payment method to continue'})
+            return res.status(400).json({ message: 'please select any payment method to continue' })
         }
-
 
         //checking from the cart
-        const cart = await cartschema.findOne({userid})
-        if (!cart || cart.length ===0) {
-            return res.status(404).json({message: ' cart is empty'})
-            
+        const cart = await cartschema.findOne({ userid })
+        if (!cart || cart.length === 0) {
+            return res.status(404).json({ message: ' cart is empty' })
         }
-        //console.log(cart.Netamount);
-        
 
         // making an ordermodel
         const ordermodel = await orderschema({
             userid,
-            products :cart.products,
-            Netamount:cart.Netamount,
-            addresses:address,
-            Status:"Pending",
+            products: cart.products,
+            Netamount: cart.Netamount,
+            addresses: address,
+            Status: "Pending",
             paymentMethod,
-            paymentStatus:"Pending",
+            paymentStatus: "Pending",
         });
 
         //saving the model
         await ordermodel.save()
-        
-        const list = await orderschema.findOne({userid})
-       
-        
+
+        //finding user from order 
+        const list = await orderschema.findOne({ userid })
 
         // Condition for cash on delivery
-        if (paymentMethod ==='COD') {
+        if (paymentMethod === 'COD') {
             const transport = nodemailer.createTransport({
-                service:"gmail",
-                secure:true,
-                auth:{
-                   user:process.env.MY_GMAIL,
-                   pass:process.env.MY_PASSWORD,
+                service: "gmail",
+                secure: true,
+                auth: {
+                    user: process.env.MY_GMAIL,
+                    pass: process.env.MY_PASSWORD,
                 }
             });
-            
+
             //NodeMailer for the cash on delivery
             const receiver = {
-                from : "abhinandh20011@gmail.com",
+                from: "abhinandh20011@gmail.com",
                 to: name.email,
-                subject : "Order Confirmation",
-                text : `hi ${name.name}, your COD order has been successfully placed rupees of ${cart.Netamount}
+                subject: "Order Confirmation",
+                text: `hi ${name.name}, your COD order has been successfully placed rupees of ${cart.Netamount}
                 the details ${list}`
             };
-            
+
             await transport.sendMail(receiver);
-            return res.status(200).json({message: 'order has been placed'})  
-           
+
+            //success message
+            return res.status(200).json({ message: 'order has been placed' })
+
         }
 
         //Condition for the online Payment
         if (paymentMethod == 'OnlinePayment') {
-          const razorpay=  instance.orders.create({
-                amount: cart.Netamount *100, 
+            const razorpay = instance.orders.create({
+                amount: cart.Netamount * 100,
                 currency: "INR",
                 receipt: ordermodel._id.toString(),
-                payment_capture: 1, 
+                payment_capture: 1,
 
 
-             //NodeMailer response throw promise function
-            }).then((response) =>{
+                //NodeMailer response throw promise function
+            }).then((response) => {
                 const transport = nodemailer.createTransport({
-                    service:"gmail",
-                    secure:true,
-                    auth:{
-                       user:process.env.MY_GMAIL,
-                       pass:process.env.MY_PASSWORD,
+                    service: "gmail",
+                    secure: true,
+                    auth: {
+                        user: process.env.MY_GMAIL,
+                        pass: process.env.MY_PASSWORD,
                     }
-                }); 
-        
+                });
+
                 const receiver = {
-                    from : "abhinandh20011@gmail.com",
+                    from: "abhinandh20011@gmail.com",
                     to: name.email,
-                    subject : "Order Confirmation",
-                    text : `hi ${name.name}, your online order has been successfully placed rupees of ${cart.Netamount}
+                    subject: "Order Confirmation",
+                    text: `hi ${name.name}, your online order has been successfully placed rupees of ${cart.Netamount}
                      the details ${list} `,
                 };
-                
-                 transport.sendMail(receiver);
-                 res.status(200).json({response:" redirect to razorpay", message: 'order has been placed'})
+
+                transport.sendMail(receiver);
+                res.status(200).json({ response: " redirect to razorpay", message: 'order has been placed' })
             })
 
-            //error message for the promise function
-            .catch((error) =>{
-                res.status(400).json(error)
-            })       
+                //error message for the promise function
+                .catch((error) => {
+                    res.status(400).json(error)
+                })
         }
 
-        // try catch error function
-        }   
-         catch (error) {
-         //error message
-         return res.status(400).json({message: 'error in create order',error:error.message
-         })
-     }   
-    
+    }
+    //error message
+    catch (error) {
+        return res.status(400).json({ message: 'error in create order', error: error.message })
+    }
+
 }
 
-        const cancelorder = async (req,res) => {
-        try {
+const cancelorder = async (req, res) => {
+    try {
         //getting the order id
         const orderid = req.params.id;
 
         //checking the order
         const order = await orderschema.findById(orderid)
+      
         //checking the order has been existing
         if (!order) {
-            return res.status(404).json({message:'order not found'})
-            
+            return res.status(404).json({ message: 'order not found' })
+
         }
 
         //only pending order will be cancel
-        if (order.Status !=='Pending') {
-            return res.status(400).json({message:'only pending order can be cancelled'})
-        }else{
-            order.Status="Cancelled"
-            res.status(400).json({message:' order has been cancelled'})
+        if (order.Status !== 'Pending') {
+            return res.status(400).json({ message: 'only pending order can be cancelled' })
+        } else {
+            order.Status = "Cancelled"
+            res.status(400).json({ message: ' order has been cancelled' })
+           
             //save the details
             await order.save()
-            
+
         }
-        
-       //error statement
+
+        //error statement
     } catch (error) {
-        return res.status(400).json({message:'error occurs'})
+        return res.status(400).json({ message: 'error occurs' })
     }
-    
+
 }
 
-const vieworder = async (req,res) => {
+const vieworder = async (req, res) => {
     try {
         //getting the user details
         const userid = req.user.id
@@ -178,21 +172,18 @@ const vieworder = async (req,res) => {
 
         //check the order is been existing
         if (!orderdetails) {
-            return res.status(404).json({message: ' order is not found'})
-            
+            return res.status(404).json({ message: ' order is not found' })
+
         }
         //order success
-        return res.status(200).json({message: 'order details',orderdetails})
-    
-    //error 
+        return res.status(200).json({ message: 'order details', orderdetails })
+
+        //error command
     } catch (error) {
-        return res.status(400).json({message: ' error',error:error.message})
+        return res.status(400).json({ message: ' error', error: error.message })
     }
-    
+
 }
 
 
-
-
-
-module.exports ={ placeanorder,cancelorder,vieworder}
+module.exports = { placeanorder, cancelorder, vieworder }
