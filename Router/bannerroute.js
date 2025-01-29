5ed72f666a11cb418d0ad3003e92dcbf026870fa
Abhinandh <abhinandh20011@gmail.com>
2025-01-29 14:47:26 +0530

    banner management complete module

--- Router/bannerroute.js
+++ Router/bannerroute.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const {createbanner,updatebanner,deletebanner} = require('../admincontroller/bannermanagement');
 const upload = require('../middleware/upload')
 const verifyToken = require('../middleware/tokenverification')
+const blacklist = require('../middleware/blacklist')
 
 
-router.post('/createbanner',upload.array('image',4),verifyToken,createbanner)
-router.put('/updatebanner/:id',verifyToken,updatebanner)
-router.delete('/deletebanner/:id',verifyToken,deletebanner)
+router.post('/createbanner',upload.array('image',4),verifyToken,blacklist,createbanner)
+router.put('/updatebanner/:id',verifyToken,blacklist,updatebanner)
+router.delete('/deletebanner/:id',verifyToken,blacklist,deletebanner)
 
 module.exports = router
--- admincontroller/bannermanagement.js
+++ admincontroller/bannermanagement.js
@@ -1,118 +1,137 @@
- const bannerschema = require('../model/banner')
+const bannerschema = require('../model/banner')
 
- const createbanner = async (req,res) => {
+const createbanner = async (req, res) => {
 
     try {
-        const {title,
+        const { title,
             description,
-            
+
         } = req.body
 
 
-         // check the feild
-         if (!title || !description ) {
+        // check the feild
+        if (!title || !description) {
             return res.status(400).json({
-            message: 'require all the feilds'
-          })
-                    
+                message: 'require all the feilds'
+            })
+
         }
-        
+
         //checking the coupon exiting 
-          const existingtitle = await bannerschema.findOne({title})
-         if (existingtitle) {
+        const existingtitle = await bannerschema.findOne({ title })
+        if (existingtitle) {
             return res.status(400).json({
-            message: 'title with this name already exists.',
-             });
-         }
-        
-         const model = await bannerschema({
+                message: 'title with this name already exists.',
+            });
+        }
+
+        //creating model
+        const model = await bannerschema({
             title,
             description,
-        
-         });
+        });
 
-         if (req.file) {
+        //multer function applying
+        if (req.file) {
             model.image = req.file.path
-            
         }
 
-        if(req.files) {
+        if (req.files) {
             let file = [];
             req.files.forEach((filename) => {
-            file.push(filename.path);                    
+                file.push(filename.path);
             });
             model.image = file;
-        
-            
         }
-        
-        
+
+        //saving the model
         await model.save()
-            return res.status(200).json({message:'bannner has been created'})
-        
-         } catch (error) {
-         //error message
-         return res.status(400).json({message: 'error in create banner',error:error.message
-             })
-         } 
-        }
-        
-        const updatebanner = async (req,res) => {
-            try {
-                const bannerid = req.params.id;
-                const {title,
-                    description,
-                    
-                } = req.body
-        
-        
-                if (!bannerid) {
-                    return res.status(404).json({message: "banner id is required"})
-                }
+        return res.status(200).json({
+            message: 'bannner has been created'
+        })
 
-                   // check the feild
-         if (!title || !description ) {
+        //error message
+    } catch (error) {
+        return res.status(400).json({
+            message: 'error in create banner',
+            error: error.message
+        })
+
+    }
+}
+
+
+
+const updatebanner = async (req, res) => {
+    try {
+        //taking from params
+        const bannerid = req.params.id;
+
+        //getting from the body
+        const { title,
+            description,
+        } = req.body
+
+        //checking the bannerID
+        if (!bannerid) {
+            return res.status(404).json({ message: "banner id is required" })
+        }
+
+        // check the feild
+        if (!title || !description) {
             return res.status(400).json({
-            message: 'require all the feilds'
-          })
-                    
-        }
-        
-                const updatebanner = await bannerschema.findByIdAndUpdate(bannerid,{title,description},{new:true});
-                if (!updatebanner) {
-                    return res.status(400).json({message: "cannot update the banner"});
-                }
-                res.status(200).json({message: "updated successfully"});
-        
-        
-                
-                 } catch (error) {
-               res.status(400).json({message: "invalid command",error:error.message})
-                
-            }
-            
-        }
-        const deletebanner = async (req,res) => {
-            
-            try {
-                const bannerid = req.params.id;
-        
-                if (!bannerid) {
-                    return res.status(404).json({message: "banner id is required"})
-                }
-                
-                const deletebanner = await bannerschema.findByIdAndDelete(bannerid)
-                if (!deletebanner) {
-                    return res.status(400).json({message: 'banner cannot been deleted'});
-                }
-                return res.status(200).json({message: 'banner deleted successfully'});
-        
-                
-            } catch (error) {
-                return res.status(404).json({message: 'invalid command'})
-                
-            }
-            
+                message: 'require all the feilds'
+            })
+
         }
 
-        module.exports = {createbanner,updatebanner,deletebanner}
+        //find from schema and updateing
+        const updatebanner = await bannerschema.findByIdAndUpdate(bannerid, { title, description }, { new: true });
+
+        //checking the function
+        if (!updatebanner) {
+            return res.status(400).json({ message: "cannot update the banner" });
+        }
+        res.status(200).json({ message: "updated successfully" });
+
+
+        //error message
+    } catch (error) {
+        res.status(400).json({ message: "invalid command", error: error.message })
+
+    }
+
+}
+
+
+
+
+const deletebanner = async (req, res) => {
+
+    try {
+        //taking from params
+        const bannerid = req.params.id;
+
+        //checking the bannerID
+        if (!bannerid) {
+            return res.status(404).json({ message: "banner id is required" })
+        }
+
+        //find from schema and delete
+        const deletebanner = await bannerschema.findByIdAndDelete(bannerid)
+
+        //checking the function
+        if (!deletebanner) {
+            return res.status(400).json({ message: 'banner cannot been deleted' });
+        }
+        return res.status(200).json({ message: 'banner deleted successfully' });
+
+        //error message
+    } catch (error) {
+        return res.status(404).json({ message: 'invalid command' })
+
+    }
+
+}
+
+module.exports = { createbanner, updatebanner, deletebanner }
