6689bd17794494c99698c3fed6223a46e0fc7ef3
Abhinandh <abhinandh20011@gmail.com>
2025-04-25 12:37:47 +0530

    Refactor  : verifytoken and blacklist has been added to the user product js

--- Router/userside-product.js
+++ Router/userside-product.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {getproduct,singleproduct} = require('../controller/productmodule')
+const verifyToken = require('../middleware/tokenverification')
+const blacklist = require('../middleware/blacklist')
 
-
-router.get('/product',getproduct)
-router.get('/product/:id',singleproduct)
+router.get('/product',verifyToken,blacklist,getproduct)
+router.get('/product/:id',verifyToken,blacklist,singleproduct)
 module.exports=router
