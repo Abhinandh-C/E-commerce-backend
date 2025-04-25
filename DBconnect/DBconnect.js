ce0350c08e2d7f8ee6e9826fdfb66982f14d96f2
Abhinandh <abhinandh20011@gmail.com>
2025-04-25 12:30:19 +0530

    Refactor : the connection string of the mongo db has been changed

--- DBconnect/DBconnect.js
+++ DBconnect/DBconnect.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+
+//function creating
 async function connectdb() {
-    await mongoose.connect('mongodb://localhost:27017/backend')
+    await mongoose.connect('mongodb+srv://abhinandh20011:lbmc6GAVPwM64Dja@ecommerce.6mcxn51.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce')
     console.log('DBrunning');
     
     
