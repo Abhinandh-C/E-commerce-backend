06169f0adeba934d6cdee0f716a56412f5cace01
Abhinandh <abhinandh20011@gmail.com>
2025-04-25 12:32:04 +0530

    Refactor : the destination of the storage has been changed

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,16 +2,16 @@ const path = require('path')
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-      destination: function (req,file,cb) {
-        cb(null,'upload/')
-      },
-      filename: function(req,file,cb) {
-        // let ext = path.extname(file.originalname)
-        // cb(null,Date.now() + ext)
-        cb(null,file.originalname)
-      }
+  destination: function (req, file, cb) {
+    cb(null, 'public/upload/')
+
+  },
+  filename: function (req, file, cb) {
+  
+    cb(null, file.originalname)
+  }
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 module.exports = upload
