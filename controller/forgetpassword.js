51ea43a8be42ec544bef4814229714408bff152f
Abhinandh <abhinandh20011@gmail.com>
2025-04-25 12:27:17 +0530

    Refactor : the forget password has been changed

--- controller/forgetpassword.js
+++ controller/forgetpassword.js
@@ -7,67 +7,63 @@ env.config()
 
 
 
-const forgetpassword = async (req,res) => {
-    try {
-        //get all data
-        console.log('hh');
-        
-        const {email} = req.body;
-        
-        //check the mail is vaild
-        if (!email) {
-          return  res.status(400).send('please provide a email ')
-        }
-        console.log('jj');
-        
-        //check the user
-        const checkuser = await userschema.findOne({ email });
+const forgetpassword = async (req, res) => {
+  try {
 
-        //check the user is already exist
-        if(!checkuser){
-          return res.status(404).send('user not found')
-        }
-        console.log('hhh');
-        
+    //taking from body
+    const { email } = req.body;
 
-        //token
-        const token = jwt.sign({email},process.env.JWT_secret_key,{
-        expiresIn: "1h",
-        });
+    //check the mail is vaild
+    if (!email) {
+      return res.status(400).send('please provide a email ')
+    }
 
-        //setting email for sent email to the client
-        const transport = nodemailer.createTransport({
-            service:"gmail",
-            secure:true,
-            auth:{
-               user:process.env.MY_GMAIL,
-               pass:process.env.MY_PASSWORD,
-            }
-        });
+    //check the user
+    const checkuser = await userschema.findOne({ email });
 
-        const receiver = {
-            from : "abhinandh20011@gmail.com",
-            to: email,
-            subject : "password Reset Request",
-            text : `Click on the link to generate your new password ${process.env.RESET_PASSWORD}${token}`,
-        };
-        
-        await transport.sendMail(receiver);
-
-        // const user = await userschema.findOne({email})
-        checkuser.token = token
-        await checkuser.save()
-      
-
-        return res.status(200).
-        send({message: "password reset link sent successfully on your gmail account",})
-        
-        } catch(error){
-            console.log(error,'error in forgetpassword');
-            
-        
-        }
+    //check the user is already exist
+    if (!checkuser) {
+      return res.status(404).send('user not found')
+    }
     
+    //token
+    const token = jwt.sign({ email }, process.env.JWT_secret_key, {
+      expiresIn: "1h",
+    });
+
+    //setting email for sent email to the client
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      auth: {
+        user: process.env.MY_GMAIL,
+        pass: process.env.MY_PASSWORD,
+      }
+    });
+
+    //sent mail
+    const receiver = {
+      from: "abhinandh20011@gmail.com",
+      to: email,
+      subject: "password Reset Request",
+      text: `Click on the link to generate your new password ${process.env.RESET_PASSWORD}${token}`,
+    };
+
+    await transport.sendMail(receiver);
+    checkuser.token = token
+
+    //save the new checkuser
+    await checkuser.save()
+
+    //success message
+    return res.status(200).
+      send({ message: "password reset link sent successfully on your gmail account", })
+
+    //error command
+  } catch (error) {
+    return res.status(500).json({message : 'error in forgetpassword'})
+  }
+
 };
 
 
