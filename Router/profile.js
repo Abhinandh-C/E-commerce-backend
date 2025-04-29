c4b3468ab03baabf88321617229296fb8b47d1b8
Abhinandh <abhinandh20011@gmail.com>
2025-04-29 17:53:40 +0530

    Refactor : changed Put method into post method and route name also been changed in the profile route

--- Router/profile.js
+++ Router/profile.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {updateprofile,viewuserprofile,deleteprofile, changepassword} = require('../controller/profilemanagement')
+const {Createprofile,viewuserprofile,deleteprofile, changepassword} = require('../controller/profilemanagement')
 const verifyToken = require('../middleware/tokenverification')
 const blacklist = require('../middleware/blacklist')
 
 router.get('/viewprofile',verifyToken,blacklist,viewuserprofile)
 router.delete('/deleteprofile',verifyToken,blacklist,deleteprofile)
-router.put('/updateprofile',verifyToken,blacklist,updateprofile)
+router.post('/Createprofile',verifyToken,blacklist,Createprofile)
 router.put('/changepassword',verifyToken,blacklist,changepassword)
 
 module.exports = router
