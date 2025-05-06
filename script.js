e6e3130c4bd3625b413a0501eb95ec7a85e79a1e
Abhinandh <abhinandh20011@gmail.com>
2025-05-06 17:31:03 +0530

    Refactor : added two local host for the frontent connect

--- script.js
+++ script.js
@@ -22,10 +22,12 @@ const cors =require('cors')
 db()
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
-    methods:['GET','POST','PUT','DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
+
 
 
 app.use('/upload', express.static('public/upload'));
