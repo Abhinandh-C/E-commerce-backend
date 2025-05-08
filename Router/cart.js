176d27662bf06b55cc407a7a4f36a478ab79264a
Abhinandh <abhinandh20011@gmail.com>
2025-05-08 17:15:22 +0530

    Refactor : added the remove all cart route in the route function

--- Router/cart.js
+++ Router/cart.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {addtocart,viewcart,removefromcart,updatecartquantity,applycoupon} = require('../controller/cartcontroller')
+const {addtocart,viewcart,removefromcart,updatecartquantity,applycoupon,removeAllCart} = require('../controller/cartcontroller')
 const verifyToken = require('../middleware/tokenverification')
 const blacklist = require('../middleware/blacklist')
 
 router.post('/addtocart/:id',verifyToken,blacklist,addtocart)
 router.get('/viewcart',verifyToken,blacklist,viewcart)
 router.delete('/removefromcart/:id',verifyToken,blacklist,removefromcart)
+router.delete('/removeAllCart',verifyToken,blacklist,removeAllCart)
 router.put('/updatecart/:id',verifyToken,blacklist,updatecartquantity)
 router.post('/applycoupon/:id',verifyToken,blacklist,applycoupon)
 
